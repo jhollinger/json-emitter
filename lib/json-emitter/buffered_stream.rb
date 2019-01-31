b8633c25cb50632dcb4ce60dea8ad4b5cf6918b5
Jordan Hollinger <jordan.hollinger@gmail.com>
2019-01-31 09:35:56 -0500

    Clone buffer

--- lib/json-emitter/buffered_stream.rb
+++ lib/json-emitter/buffered_stream.rb
@@ -66,8 +66,8 @@ module JsonEmitter
         @enum.each { |str|
           buff << str
           if buff.bytesize >= @buffer_size
-            y << buff
-            buff = ""
+            y << buff.clone
+            buff.clear
           end
         }
         y << buff unless buff.empty?
